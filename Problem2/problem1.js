ef7a2f4241d5918add2ce32de9f8eea67e8ef2e3
furusetomoya <tomo.fwork18@gmail.com>
2022-03-06 00:25:34 +0900

    add Problem2 problem1

---
+++ Problem2/problem1.js
@@ -0,0 +1,25 @@
+let n = Number(process.argv[3].split(" ")[0]);
+let N = Number(process.argv[3].split(" ")[1]);
+let inputWater = process.argv[4].split(" ");
+
+let smallNum = false;
+let bigNum = false;
+
+for (let i = 0; i < inputWater.length; i++) {
+  let curr = Number(inputWater[i]);
+  if (curr === N) {
+    console.log("Yes");
+    return;
+  } else if (curr < N) {
+    smallNum = true;
+  } else {
+    bigNum = true;
+  }
+
+  if (bigNum === true && smallNum === true) {
+    console.log("Yes");
+    return;
+  }
+}
+
+console.log("No");
