0f68f0172278a2fb93b79f6ce084b33692baad44
furusetomoya <tomo.fwork18@gmail.com>
2022-03-06 11:38:25 +0900

    problem1.js problem2.js finished

--- Problem1/node/index.js
+++ Problem1/node/index.js
@@ -5,16 +5,16 @@ const server = http.createServer((req, res)=>{
    const queryObject = url.parse(req.url, true).query;
    console.log(queryObject.obj);
 
-   let hashmap = {}
-   const obj = queryObject.obj;
+   // let hashmap = {}
+   // const obj = queryObject.obj;
 
-   for (let i = 0; i < obj.length; i++) {
-      let curr = obj[i];
-      hashmap[curr.num] = curr.text;
-   }
+   // for (let i = 0; i < obj.length; i++) {
+   //    let curr = obj[i];
+   //    hashmap[curr.num] = curr.text;
+   // }
 
    // ここに処理を記述してください。
-   ans = "answer";
+   let ans = "answer";
    let result = "";
    for (let i = 1; i <= 30; i++) {
       if (i % 15 === 0) result += "Fizz Buzz, ";
@@ -30,7 +30,7 @@ const server = http.createServer((req, res)=>{
    res.writeHead(200, {'Content-Type': 'text/html'});
    res.write(ans);
    res.write(result);
-   res.hashmap(hashmap);
+   // res.hashmap(hashmap);
    res.end();
 });
 server.listen(8080);
