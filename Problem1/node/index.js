a6e814d8f2cf3f696eb8bfce7166a181e014db7a
furusetomoya <tomo.fwork18@gmail.com>
2022-03-06 13:19:16 +0900

    Problem1 finished

--- Problem1/node/index.js
+++ Problem1/node/index.js
@@ -5,32 +5,13 @@ const server = http.createServer((req, res)=>{
    const queryObject = url.parse(req.url, true).query;
    console.log(queryObject.obj);
 
-   // let hashmap = {}
-   // const obj = queryObject.obj;
-
-   // for (let i = 0; i < obj.length; i++) {
-   //    let curr = obj[i];
-   //    hashmap[curr.num] = curr.text;
-   // }
-
-   // ここに処理を記述してください。
-   let ans = "answer";
-   let result = "";
-   for (let i = 1; i <= 30; i++) {
-      if (i % 15 === 0) result += "Fizz Buzz, ";
-      else if (i % 5 === 0) result += "Buzz, ";
-      else if (i % 3 === 0) result += "Fizz, ";
-      else result += i + ", ";
-   }
-
    res.setHeader('Access-Control-Allow-Origin', '*');
    res.setHeader('Access-Control-Request-Method', '*');
    res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST');
    res.setHeader('Access-Control-Allow-Headers', '*');
    res.writeHead(200, {'Content-Type': 'text/html'});
    res.write(ans);
-   res.write(result);
-   // res.hashmap(hashmap);
+   res.write(num);
    res.end();
 });
 server.listen(8080);
--- Problem1/php/index.php
+++ Problem1/php/index.php
@@ -5,7 +5,25 @@ header("Access-Control-Allow-Headers: *");
 $obj = $_POST['pattern'];
 
 // ここに処理を記述してください。
+$hashmapJson = json_decode($obj, true);
+$hashmap = $hashmapJson["obj"];
+$result = "";
+for ($i = 1; $i <= 30; $i++) {
+    $curr = "";
+    for ($j = 0; $j < count($hashmap); $j++) {
+        if ($i % (int)$hashmap[$j]["num"] === 0) {
+            $curr .= $hashmap[$j]["text"] . " ";
+        }
+    }
+    if($curr !== "") {
+        $curr = substr($curr, 0, -1);
+        $result .= $curr . ", ";
+    } else {
+        $result .= strval($i) . ", ";
+    }
+}
 
-$ans = "answer";
-echo($ans);
+$result = substr($result, 0, -2);
+
+echo($result);
 ?>
